480e4fef83c2785bf8112f42cf9db7a029c0ec64
Jeff Rodriguez <52290399+xjefrod99@users.noreply.github.com>
2020-08-14 23:15:00 -0400

    Add files via upload
    beginning to create an applet

---
+++ SampleApplet.java
@@ -0,0 +1,14 @@
+import java.applet.Applet;
+import java.awt.*;
+import java.io.*;
+
+public class SampleApplet extends Applet{
+
+    public void paint(Graphics g){
+        g.setColor(Color.red);
+        g.drawRect(10,10,100,100); //width 250, height 100
+        g.setColor(Color.blue);
+        g.drawString("Trying this out",10,50);
+
+    }
+}
