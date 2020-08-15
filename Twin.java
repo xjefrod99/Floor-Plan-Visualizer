2af0f670357f7eeeec2c45d0adac5f5127333077
Jake Gaughan <mgaughan@middlebury.edu>
2020-08-15 16:13:54 -0700

    working on new twin bed object

---
+++ Twin.java
@@ -0,0 +1,10 @@
+import java.applet.Applet;
+import java.awt.*;
+import java.io.*;
+
+public class Twin extends Applet{
+    public void BedPaint(Graphics g){
+        g.setColor(Color.blue);
+        g.fillRect(200,100,100,100);
+    }
+}
