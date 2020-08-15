130400687adbe9718533336be96913310c64b7b1
Jake Gaughan <mgaughan@middlebury.edu>
2020-08-15 16:14:15 -0700

    updating twin bed object and room

--- SampleApplet.java
+++ SampleApplet.java
@@ -2,14 +2,17 @@ import java.applet.Applet;
 import java.awt.*;
 import java.io.*;
 
+
 public class SampleApplet extends Applet{
 
     public void paint(Graphics g){
         //setForeground(Color.red);
+        Twin Bed = new Twin();
         g.setColor(Color.orange);
         g.fillRect(200,100,500,500); //scale: 500 = 10ft; 50 = 1ft
-        g.setColor(Color.blue);
+        //g.setColor(Color.blue);
         //g.drawString("editings",10,50);
+        Bed.BedPaint(g);
 
     }
 }
