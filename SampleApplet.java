95799e3baa7c286b296a809e2eee66417fcb1216
Jake Gaughan <mgaughan@middlebury.edu>
2020-08-15 16:30:19 -0700

    pillow and blanket on the bed

--- SampleApplet.java
+++ SampleApplet.java
@@ -8,11 +8,12 @@ public class SampleApplet extends Applet{
     public void paint(Graphics g){
         //setForeground(Color.red);
         Twin Bed = new Twin();
+        //this is the room
         g.setColor(Color.orange);
         g.fillRect(200,100,500,500); //scale: 500 = 10ft; 50 = 1ft
         //g.setColor(Color.blue);
         //g.drawString("editings",10,50);
-        Bed.BedPaint(g);
+        Bed.BedPaint(g, 200, 100);
 
     }
 }
--- Twin.java
+++ Twin.java
@@ -3,8 +3,17 @@ import java.awt.*;
 import java.io.*;
 
 public class Twin extends Applet{
-    public void BedPaint(Graphics g){
+    public void BedPaint(Graphics g, int x, int y){
+        //twin XL are 38in X 80in
         g.setColor(Color.blue);
-        g.fillRect(200,100,100,100);
+        g.fillRect(x,y,158,333); //so the w = 158.33 and l = 333.33 but trying to figure out how to pass a double to fillRect
+        g.setColor(Color.black);
+        g.drawRect(x,y,158,333);
+        //pillow
+        g.setColor(Color.pink);
+        g.fillRect((x + 30),y + 10,100,64);
+        //blanket
+        g.setColor(Color.pink);
+        g.fillRect(x,y + 85,158,248);
     }
 }
