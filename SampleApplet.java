9bb80862fb0135791bb2470e050ef7642e47123b
Jake Gaughan <mgaughan@middlebury.edu>
2020-08-15 16:41:58 -0700

    updating some commenting

--- SampleApplet.java
+++ SampleApplet.java
@@ -6,13 +6,13 @@ import java.io.*;
 public class SampleApplet extends Applet{
 
     public void paint(Graphics g){
-        //setForeground(Color.red);
+        //creating a new TwinXL bed
         Twin Bed = new Twin();
         //this is the room
         g.setColor(Color.orange);
         g.fillRect(200,100,500,500); //scale: 500 = 10ft; 50 = 1ft
-        //g.setColor(Color.blue);
-        //g.drawString("editings",10,50);
+        
+        //drawing the twinXL bed at a specified location
         Bed.BedPaint(g, 200, 100);
 
     }
